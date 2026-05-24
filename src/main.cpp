1493e5a9310a7d11c658d943fc36862033ba01b3
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-24 19:26:33 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,17 @@
 #include <Arduino.h>
-#include <SPI.h>
 #include <Arduino_GFX_Library.h>
-
-#define BLACK     0x0000
-#define WHITE     0xFFFF
-#define RED       0xF800
-#define GREEN     0x07E0
-#define BLUE      0x001F
-#define CYAN      0x07FF
-#define YELLOW    0xFFE0
-#define MAGENTA   0xF81F
-#define DARKGREY  0x7BEF
-
-// ======================
-// WAVESHARE ESP32-C6 LCD 1.47
-// ======================
 
 #define TFT_BL    15
 
 #define TFT_MOSI  6
 #define TFT_SCLK  7
-#define TFT_CS    14
 #define TFT_DC    8
+#define TFT_CS    14
 #define TFT_RST   9
 
-// ======================
-// DISPLAY
-// ======================
+#define BLACK 0x0000
+#define WHITE 0xFFFF
+#define RED   0xF800
 
 Arduino_DataBus *bus = new Arduino_ESP32SPI(
     TFT_DC,
@@ -39,27 +24,23 @@ Arduino_DataBus *bus = new Arduino_ESP32SPI(
 Arduino_GFX *gfx = new Arduino_ST7789(
     bus,
     TFT_RST,
-    1,      // rotation
-    true,   // IPS
-    172,    // width
-    320,    // height
-    34,     // col offset 1
-    0,      // row offset 1
-    34,     // col offset 2
-    0       // row offset 2
+    1,
+    true,
+    172,
+    320,
+    34,
+    0,
+    34,
+    0
 );
 
 void setup()
 {
     Serial.begin(115200);
 
-    // BACKLIGHT
     pinMode(TFT_BL, OUTPUT);
     digitalWrite(TFT_BL, HIGH);
 
-    delay(200);
-
-    // DISPLAY
     gfx->begin();
 
     gfx->fillScreen(BLACK);
@@ -68,14 +49,9 @@ void setup()
     gfx->setTextSize(2);
 
     gfx->setCursor(20, 40);
-    gfx->println("LCD TEST");
-
-    gfx->setCursor(20, 80);
-    gfx->println("ESP32-C6");
+    gfx->println("DISPLAY OK");
 
     gfx->drawRect(0, 0, 320, 172, RED);
-
-    Serial.println("DISPLAY OK");
 }
 
 void loop()
