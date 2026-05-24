2dba8c5f668b43bd249adccacb8caf005c7288d1
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-24 18:26:31 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,32 +13,23 @@
 #define CYAN    0x07FF
 
 // ======================================================
-// LCD PIN WAVESHARE ESP32-C6 1.47
+// WAVESHARE ESP32-C6 LCD 1.47
 // ======================================================
 
 #define TFT_BL    15
 
-#define TFT_MOSI  6
-#define TFT_SCLK  7
 #define TFT_DC    8
 #define TFT_CS    14
 #define TFT_RST   21
 
 // ======================================================
-// DISPLAY BUS
+// SPI DISPLAY
 // ======================================================
 
-Arduino_DataBus *bus = new Arduino_ESP32SPI(
+Arduino_DataBus *bus = new Arduino_HWSPI(
     TFT_DC,
-    TFT_CS,
-    TFT_SCLK,
-    TFT_MOSI,
-    GFX_NOT_DEFINED
+    TFT_CS
 );
-
-// ======================================================
-// DISPLAY
-// ======================================================
 
 Arduino_GFX *gfx = new Arduino_ST7789(
     bus,
@@ -61,12 +52,18 @@ void setup()
 {
     Serial.begin(115200);
 
+    // =========================
     // BACKLIGHT
+    // =========================
 
     pinMode(TFT_BL, OUTPUT);
+
+    // COBA HIGH DULU
     digitalWrite(TFT_BL, HIGH);
 
+    // =========================
     // LCD RESET
+    // =========================
 
     pinMode(TFT_RST, OUTPUT);
 
@@ -79,15 +76,21 @@ void setup()
     digitalWrite(TFT_RST, HIGH);
     delay(200);
 
+    // =========================
     // DISPLAY INIT
+    // =========================
 
     gfx->begin();
 
-    gfx->fillScreen(BLACK);
+    gfx->invertDisplay(true);
 
     gfx->setRotation(1);
 
-    // TEST BORDER
+    // =========================
+    // TEST SCREEN
+    // =========================
+
+    gfx->fillScreen(BLACK);
 
     gfx->drawRect(
         0,
@@ -97,7 +100,9 @@ void setup()
         CYAN
     );
 
-    // TEST TEXT
+    // =========================
+    // TEXT
+    // =========================
 
     gfx->setTextColor(WHITE);
 
@@ -112,13 +117,15 @@ void setup()
     gfx->setCursor(20, 120);
     gfx->println("WAVESHARE 1.47");
 
-    // TEST COLORS
+    // =========================
+    // COLOR TEST
+    // =========================
 
-    gfx->fillRect(250, 30, 40, 40, RED);
+    gfx->fillRect(250, 20, 40, 40, RED);
 
-    gfx->fillRect(250, 80, 40, 40, GREEN);
+    gfx->fillRect(250, 70, 40, 40, GREEN);
 
-    gfx->fillRect(250, 130, 40, 40, BLUE);
+    gfx->fillRect(250, 120, 40, 40, BLUE);
 
     Serial.println("LCD TEST DONE");
 }
