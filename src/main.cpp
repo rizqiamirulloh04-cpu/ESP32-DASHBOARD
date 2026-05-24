91812e863a7de4a93e71b97d737e36d68628dd4f
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-24 19:08:43 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,16 @@
 #include <SPI.h>
 #include <Arduino_GFX_Library.h>
 
+#define BLACK     0x0000
+#define WHITE     0xFFFF
+#define RED       0xF800
+#define GREEN     0x07E0
+#define BLUE      0x001F
+#define CYAN      0x07FF
+#define YELLOW    0xFFE0
+#define MAGENTA   0xF81F
+#define DARKGREY  0x7BEF
+
 // ======================
 // WAVESHARE ESP32-C6 LCD 1.47
 // ======================
