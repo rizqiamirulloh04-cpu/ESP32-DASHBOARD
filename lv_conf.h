cad9e64de6990e981b2498427e7937e1026deafe
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-24 14:57:30 +0700

    Create lv_conf.h

---
+++ lv_conf.h
@@ -0,0 +1,22 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_COLOR_DEPTH 16
+
+#define LV_USE_LOG 0
+
+#define LV_USE_LABEL 1
+#define LV_USE_BTN 1
+#define LV_USE_BAR 1
+#define LV_USE_SLIDER 1
+
+#define LV_USE_ANIMATION 1
+
+#define LV_USE_THEME_DEFAULT 1
+
+#define LV_FONT_MONTSERRAT_12 1
+#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_16 1
+#define LV_FONT_MONTSERRAT_20 1
+
+#endif
