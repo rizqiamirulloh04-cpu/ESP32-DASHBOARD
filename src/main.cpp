a8e583a207a2cc0ecb928f743e08735dc3669f31
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-25 18:13:48 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -58,7 +58,10 @@ void setup()
 
 void loop()
 {
-    // ===== SPEED UPDATE =====
+    static int steering = -100;
+    static int steeringDir = 1;
+
+    // ===== SPEED =====
 
     speedValue += dir * 2;
 
@@ -72,9 +75,60 @@ void loop()
         dir = 1;
     }
 
-    // ===== CLEAR SPEED ONLY =====
+    // ===== STEERING ANIMATION =====
+
+    steering += steeringDir * 8;
+
+    if (steering >= 100)
+    {
+        steeringDir = -1;
+    }
+
+    if (steering <= -100)
+    {
+        steeringDir = 1;
+    }
+
+    // ===== CLEAR SPEED AREA =====
 
     gfx->fillRect(60, 35, 210, 60, 0x0000);
+
+    // ===== CLEAR ARROW AREA =====
+
+    gfx->fillRect(0, 45, 55, 50, 0x0000);
+    gfx->fillRect(265, 45, 55, 50, 0x0000);
+
+    // ===== LEFT ARROW =====
+
+    uint16_t leftColor = 0x4208;
+
+    if (steering < -20)
+    {
+        leftColor = 0x07E0;
+    }
+
+    gfx->fillTriangle(
+        20, 70,
+        45, 55,
+        45, 85,
+        leftColor
+    );
+
+    // ===== RIGHT ARROW =====
+
+    uint16_t rightColor = 0x4208;
+
+    if (steering > 20)
+    {
+        rightColor = 0x07E0;
+    }
+
+    gfx->fillTriangle(
+        300, 70,
+        275, 55,
+        275, 85,
+        rightColor
+    );
 
     // ===== DRAW SPEED =====
 
@@ -102,10 +156,8 @@ void loop()
 
     int bar = map(speedValue, 0, 120, 0, 240);
 
-    // clear previous bar
     gfx->fillRect(40, 145, 240, 12, 0x2104);
 
-    // draw new bar
     gfx->fillRect(40, 145, bar, 12, 0xF800);
 
     delay(20);
