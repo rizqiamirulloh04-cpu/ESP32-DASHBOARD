ddbad38c626c46aa2df5fbaf2dae444130fdaec6
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-25 23:17:19 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
 
+#define BLACK   0x0000
+#define WHITE   0xFFFF
+#define RED     0xF800
+#define GREEN   0x07E0
+#define BLUE    0x001F
+#define CYAN    0x07FF
+
 // ======================
 // WAVESHARE ESP32-C6 LCD 1.47"
 // ======================
