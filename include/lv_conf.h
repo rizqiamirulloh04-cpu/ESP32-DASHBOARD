f3b7eb139761885e4b7f35d8dd03e62a3d11bd2b
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-05-26 16:03:56 +0700

    Create include/lv_conf.h

---
+++ include/lv_conf.h
@@ -0,0 +1,21 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_COLOR_DEPTH 16
+
+#define LV_MEM_SIZE (64U * 1024U)
+
+#define LV_USE_LOG 0
+
+#define LV_USE_ASSERT_NULL 1
+#define LV_USE_ASSERT_MALLOC 1
+
+#define LV_FONT_MONTSERRAT_18 1
+#define LV_FONT_MONTSERRAT_20 1
+#define LV_FONT_MONTSERRAT_48 1
+
+#define LV_USE_ARC 1
+#define LV_USE_BAR 1
+#define LV_USE_LABEL 1
+
+#endif
