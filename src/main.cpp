b6c1fc707c9c5a5fa4724383eeb4118af1438d7b
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-06-05 21:30:42 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,25 @@
 #include <Arduino_GFX_Library.h>
 #include "ui_classic.h"
 
-// Definisi Pin
-#define TFT_BL 22
-// ... (tambahkan definisi pin lainnya)
-
-// DEFINISI VARIABEL (Hanya di satu tempat ini)
+// Definisi variabel global (Hanya di sini)
 Arduino_DataBus *bus = new Arduino_ESP32SPI(15, 14, 7, 6, GFX_NOT_DEFINED);
 Arduino_GFX *gfx = new Arduino_ST7789(bus, 21, 1, true, 172, 320, 34, 0, 34, 0);
 Arduino_Canvas *canvas = new Arduino_Canvas(320, 172, gfx);
 
 void setup() {
+    // Isolasi Pin agar tidak mengganggu booting
+    pinMode(1, INPUT_PULLUP);
+    pinMode(2, INPUT_PULLUP);
+    
     Serial.begin(115200);
-    // ... setup lainnya
     gfx->begin();
     canvas->begin();
-    delay(1000);
+    delay(1000); 
 }
 
 void loop() {
-    // Panggil fungsi dari ui_classic.h di sini
+    // Panggil fungsi dari ui_classic.cpp
     draw_ui_classic(10, 20, 89, -67, 38, true);
     gfx->draw16bitRGBBitmap(0, 0, canvas->getFramebuffer(), 320, 172);
+    delay(3);
 }
