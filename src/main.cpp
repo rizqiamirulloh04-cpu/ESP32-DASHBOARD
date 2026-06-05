78508efec380ffe476fda2154318ad18d695092b
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-06-05 14:20:52 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,11 @@
 #include "ui_classic.h"
 #include <Preferences.h>
 
-// 1. Tambahkan deklarasi variabel yang dibutuhkan
-int speedValue = 0;
-int rpmBarWidth = 0;
-int batteryPercent = 89;
-int signalDbm = -67;
-int currentSteerState = 0;
-
+// Deklarasi global agar bisa diakses ui_classic.cpp via extern
 Arduino_DataBus *bus = new Arduino_ESP32SPI(15, 14, 7, 6, -1);
 Arduino_GFX *gfx = new Arduino_ST7789(bus, 21, 1, true, 172, 320, 34, 0, 34, 0);
 
+int speedValue = 0, rpmBarWidth = 0, batteryPercent = 89, signalDbm = -67, currentSteerState = 0;
 Preferences prefs;
 int topSpeed = 0;
 
@@ -24,16 +19,10 @@ void setup() {
 }
 
 void loop() {
-    // 2. Di sini tempat kamu memasukkan logika pembacaan sensor (pulseIn, map, dll.)
-    // Contoh singkat:
-    // speedValue = map(analogRead(THROTTLE_PIN), 0, 4095, 0, 120);
-    
     static unsigned long blinkTimer = 0;
     static bool blinkState = false;
     if (millis() - blinkTimer > 350) { blinkTimer = millis(); blinkState = !blinkState; }
 
-    // Sekarang variabel sudah dideklarasikan, error akan hilang
     draw_ui_classic(speedValue, rpmBarWidth, batteryPercent, signalDbm, currentSteerState, blinkState);
-    
     delay(10);
 }
