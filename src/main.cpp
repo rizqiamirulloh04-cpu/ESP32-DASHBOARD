2f81164a8e53bdf78cc5a46ad8886f44cba4b917
rizqiamirulloh04-cpu <rizqiamirulloh04@gmail.com>
2026-06-05 14:52:14 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,54 @@
 #include <Arduino.h>
 #include <Arduino_GFX_Library.h>
+#include "ui_classic.h"
 
-// Definisi pin sesuai hardware asli kamu
+// --- KONFIGURASI HARDWARE WAVESHARE ESP32-C6 1.47" ---
 #define TFT_BL 22
 #define TFT_MOSI 6
 #define TFT_SCLK 7
-#define TFT_CS   14
-#define TFT_DC   15
-#define TFT_RST  21
+#define TFT_CS 14
+#define TFT_DC 15
+#define TFT_RST 21
 
+// Inisialisasi Bus SPI
 Arduino_DataBus *bus = new Arduino_ESP32SPI(TFT_DC, TFT_CS, TFT_SCLK, TFT_MOSI, GFX_NOT_DEFINED);
-Arduino_GFX *gfx = new Arduino_ST7789(bus, TFT_RST, 1, true, 172, 320, 34, 0, 34, 0);
+
+// Inisialisasi Layar
+Arduino_GFX *gfx = new Arduino_ST7789(bus, TFT_RST, 3, true, 172, 320, 34, 0, 34, 0);
+
+// --- VARIABEL DATA ---
+int speedValue = 0;
+int rpmValue = 0;
+int batteryPercent = 89;
+int signalDbm = -67;
+int steerState = 0; 
 
 void setup() {
     Serial.begin(115200);
-    
-    // Paksa backlight nyala
-    pinMode(TFT_BL, OUTPUT);
-    digitalWrite(TFT_BL, HIGH); 
 
+    // Aktifkan Backlight
+    pinMode(TFT_BL, OUTPUT);
+    digitalWrite(TFT_BL, HIGH);
+
+    // Inisialisasi Layar & UI
     if (!gfx->begin()) {
-        Serial.println("gfx->begin() gagal!");
-    } else {
-        Serial.println("gfx->begin() sukses!");
-        gfx->fillScreen(0xF800); // Merah (0xF800 adalah warna merah di RGB565)
-        Serial.println("Seharusnya layar sekarang berwarna merah.");
+        Serial.println("Gagal menginisialisasi layar!");
     }
+    init_ui(gfx);
+    
+    Serial.println("System Ready - Menampilkan UI...");
 }
 
 void loop() {
-    // Kosongkan
+    static unsigned long blinkTimer = 0;
+    static bool blinkState = false;
+    if (millis() - blinkTimer > 400) {
+        blinkTimer = millis();
+        blinkState = !blinkState;
+    }
+
+    // Panggil fungsi UI dari file ui_classic.cpp
+    draw_ui_classic(speedValue, rpmValue, batteryPercent, signalDbm, steerState, blinkState);
+
+    delay(10);
 }
